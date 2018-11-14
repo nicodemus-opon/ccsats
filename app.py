397ff732a35c6de70afd12d3af4b19aab2d71893
Your Name <you@example.com>
2018-11-15 00:59:03 +0300

    bench

--- app.py
+++ app.py
@@ -251,7 +251,7 @@ def test():
     for the_file in os.listdir(folder):
         file_path = os.path.join(folder, the_file)
         try:
-            if os.path.isfile(file_path) and the_file!="template.docx":
+            if os.path.isfile(file_path) and the_file!="template.docx" and the_file!="main.txt":
                 os.unlink(file_path)
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
